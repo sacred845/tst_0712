56b1028fba1db5f91782537f8c664351155fd03d
root <root@89-108-98-99.cloudvps.regruhosting.ru>
2023-12-08 08:24:12 +0300

    Add text of task

---
+++ src/Controllers/TaskController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Response;
+use App\Controller;
+use App\Entity\Dashboard;
+use App\Entity\Course;
+use App\Core;
+
+class TaskController extends Controller
+{
+    public function tst_0712()
+    {
+        return new Response('task/tst_0712');
+    }
+}
